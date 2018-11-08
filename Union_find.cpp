f0f0dc65f3ad14e6d0e6588db42d2d8511418e1c
Alice <alice@wirelessprv-10-195-102-47.near.illinois.edu>
2018-11-07 19:34:20 -0600

    implemented union find template

---
+++ Union_find.cpp
@@ -0,0 +1,44 @@
+//
+//  Union_find.cpp
+//  
+//
+//  Created by Alice on 11/7/18.
+//
+
+class Unionfind{
+public:
+    int union_count;
+    vector<int> id;
+    vector<int> size;
+    
+    Unionfind(int len){
+        union_count = len;
+        id = vector<int> (len);
+        size = vector<int> (len);
+    }
+    
+    bool find(int p, int q){
+        return root(p) == root(q);
+    }
+    
+    void unions(int p, int q){
+        int pi = root(p), qi = root(q);
+        if (size[pi] < size[qi]){
+            id[pi] = qi;
+            size[qi] += size[pi];
+        }
+        else{
+            id[qi] = pi;
+            size[pi] += size[qi];
+        }
+        union_count--;
+    }
+    
+    int root(int i){
+        while (i != id[i]){
+            id[i] = id[id[i]];
+            i = id[i];
+        }
+        return i;
+    }
+};
